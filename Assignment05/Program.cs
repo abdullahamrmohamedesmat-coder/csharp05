5a0936dff46864acac23310aeaec3e1c31cba748
abdullah amr <abdullahamrmohamedesmat@@gmail.com>
2026-09-06 02:45:16 +0300

    1st answer

--- Assignment05/Program.cs
+++ Assignment05/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-
             #region question 1
             //1st answer
             Book book = new Book();
 
             //Console.WriteLine(book.password;);  it don't allow to print it because access modifer for password is private 
             #endregion
+
         }
     }
 }
