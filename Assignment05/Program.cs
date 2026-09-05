06f1561626d6d99a8abf55bfea3920b4b8c0fcff
abdullah amr <abdullahamrmohamedesmat@@gmail.com>
2026-09-06 02:43:59 +0300

    Add project files.

---
+++ Assignment05/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region question 1
+            //1st answer
+            Book book = new Book();
+
+            //Console.WriteLine(book.password;);  it don't allow to print it because access modifer for password is private 
+            #endregion
+        }
+    }
+}
